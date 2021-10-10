9b3d0fb30d14cebfbc51b346c33c42f527ca6f37
Alsush10 <alsu.mannanova33@mail.ru>
2021-10-10 17:49:39 +0500

    Update HomeWork1.java

--- HomeWork1.java
+++ HomeWork1.java
@@ -3,14 +3,34 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        byte arg_byte = 5;
+        byte typeByte = 127; //-128
+        System.out.println("Max byte = " + typeByte);
+        short typeShort = 32767; //-32768
+        System.out.println("Max short = " + typeShort);
+        int typeInt = 2147483647; // -2147483648
+        System.out.println("Max int = " + typeInt);
+        long typeLong = 9223372036854775807L; //-9223372036854775808
+        System.out.println("Max long = " + typeLong);
+        float typeFloat = 31.4f; //f - тк иначе считает как double
+        System.out.println("float: " + typeFloat);
+        double typeDouble = 3.141;
+        System.out.println("double: " + typeDouble);
+        char typeChar = 'ы', ch = 102; // 102 - символ 'f'
+        System.out.println("char: = " + typeChar + " " + ch);
+        boolean typeBoolean = true; //false
+        System.out.println("boolean: " + typeBoolean);
 
-        System.out.println(Task_1(2, 3, 6, 2));
-        System.out.println(Task_2(5, 7));
+
+        String typeString = "Hello \nworld";
+        System.out.println("№1: " + Task_1(2, 3, 6, 2));
+        System.out.println("№2: " + Task_2(5, 7));
+        System.out.print("№3: ");
         Task_3(5);
-        System.out.println(Task_4(-7));
+        System.out.println("№4: " + Task_4(-7));
+        System.out.print("№5: ");
         Task_5("Алсу");
-        Task_6(2100);
+        System.out.print("№6: ");
+        Task_6(2000);
     }
 
     static double Task_1(int a, int b, int c, int d){
